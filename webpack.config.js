50511a68e2f8e4d43cad2d67c004f94d901ab5e7
lichen105519 <lichen105519@pwrd.com>
2017-06-30 14:14:20 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,47 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackDevServer = require('webpack-dev-server');
-module.exports = {
-    entry:{
-        app:__dirname + '/src/index.jsx',
-        vendor:['react','react-dom']
-    },
-    output:{
-        path:__dirname + '/dist/',
-        publicPath:'/dist/',
-        filename:'js/[name].js'
-    },
-    module:{
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['react']
+
+module.exports = (env) =>{
+    let _publicPath = env === 'dev' ? '' : '/dist/';
+    return {
+        entry:{
+            app:__dirname + '/src/index.jsx',
+            vendor:['react','react-dom']
+        },
+        output:{
+            path:__dirname + '/dist/',
+            publicPath:_publicPath,
+            filename:'js/[name].js'
+        },
+        module:{
+            loaders: [
+                {
+                    test: /\.jsx?$/,
+                    loader: 'babel-loader',
+                    exclude: /node_modules/,
+                    query: {
+                        presets: ['react']
+                    }
                 }
-            }
-        ]
-    },
-    devServer:{
-        contentBase:'http://localhost:8080/dist',
-        historyApiFallback:true,
-        hot:true,
-        inline:true,
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            title:'react',
-            template:__dirname + '/html/index.html',
-            filename:'index.html',
-            hash:true,
-            inject:'body',
-            chunks:['vendor','app'],
-        }),
-        new webpack.optimize.CommonsChunkPlugin('vendor'),
-        new webpack.HotModuleReplacementPlugin()
-    ],
+            ]
+        },
+        devServer:{
+            historyApiFallback:true,
+            hot:true,
+            inline:true,
+        },
+        plugins:[
+            new HtmlWebpackPlugin({
+                title:'react',
+                template:__dirname + '/html/index.html',
+                filename:'index.html',
+                hash:true,
+                inject:'body',
+                chunks:['vendor','app'],
+            }),
+            new webpack.optimize.CommonsChunkPlugin('vendor'),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+    }
 }
