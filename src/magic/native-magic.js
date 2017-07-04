03da21a3fce404a92b2db9e2b38febc372c4602f
lichen105519 <lichen105519@pwrd.com>
2017-07-04 10:35:50 +0800

    添加原生构造器原型拓展

--- src/magic/native-magic.js
+++ src/magic/native-magic.js
@@ -1,6 +1,7 @@
 //create 17,07,04 lc
-//纯属瞎折腾
+//纯属瞎折腾---划擦已划擦
 export default (() => {
+    //Function
     Function.prototype.bind = function(){
         let args = [].slice.call(arguments);
         let self = args.shift();
@@ -9,7 +10,7 @@ export default (() => {
             let nextArgs = [].slice.call(arguments);
             return fn.apply(self,args.concat(nextArgs));
         }
-    }
+    };
     Function.prototype.before = function(fnc){
         let fn = this;
         return function(){
@@ -18,7 +19,7 @@ export default (() => {
             }
             return  fn.apply(this,arguments);
         }
-    }
+    };
     Function.prototype.after = function(fnc){
         let fn = this;
         return function(){
@@ -27,6 +28,47 @@ export default (() => {
                 fnc();
             }
             return result;
+        }
+    };
+    //Array
+    Array.prototype.find = function(fnc){
+        if(typeof fnc !== 'function'){
+            return null;
+        }
+        let arr = this;
+        let result = null;
+        for (let i = 0,len = arr.length; i < len; i++){
+            if(fnc(arr[i]) === true){
+                result = arr[i];
+                break;
+            }
+        }
+        return result;
+    };
+    Array.prototype.findKey = function(fnc){
+        if(typeof fnc !== 'function'){
+            return null;
+        }
+        let arr = this;
+        let result = -1;
+        for (let i = 0,len = arr.length; i < len; i++){
+            if(fnc(arr[i]) === true){
+                result = i;
+                break;
+            }
+        }
+        return result;
+    };
+    Array.prototype.each = function(fnc){
+        //forEach，map不能break 不好用
+        if(typeof fnc !== 'function'){
+            return null;
+        }
+        let arr = this;
+        for (let i = 0,len = arr.length; i < len; i++){
+            if(fnc(arr[i]) === false){
+                break;
+            }
         }
     }
 })();
